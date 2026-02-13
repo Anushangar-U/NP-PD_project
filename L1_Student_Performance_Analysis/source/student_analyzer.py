225349bda699d538597e21a3bf4f1bf4c1d096a5
Anushangar-U <u.anushangar@gmail.com>
2026-02-13 19:10:44 +0530

    cleaning the data of student csv

--- L1_Student_Performance_Analysis/source/student_analyzer.py
+++ L1_Student_Performance_Analysis/source/student_analyzer.py
@@ -11,4 +11,7 @@ df = df.fillna({"Assignment_Mark":AssignmentMarkAvg,"Exam_Mark":ExamMarkAvg})
 
 print(df.to_string)
 
-
+#converting columns to arrays
+Student_ID_arr = df["Student_ID"].values
+Name_arr = df["Name"].values
+Student_ID_arr = df["Student_ID"].values
