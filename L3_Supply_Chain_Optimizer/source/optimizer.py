d54ca728fb37b1cc42cf01a4b868a3885b0c219b
Anushangar-U <u.anushangar@gmail.com>
2026-02-24 20:01:05 +0530

    adding revenue column

--- L3_Supply_Chain_Optimizer/source/optimizer.py
+++ L3_Supply_Chain_Optimizer/source/optimizer.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas as pd
 
+#importing csv into dataframe
 df_i = pd.read_csv(r"python\numpy_project\L3_Supply_Chain_Optimizer\data\inventory.csv")
 df_t = pd.read_csv(r"python\numpy_project\L3_Supply_Chain_Optimizer\data\transactions.csv")
 
-merged_df = pd.merge(df_i,df_t,on="Product_ID")
+#merging 2 df into 1
+merged_df = pd.merge(df_i,df_t,on="Product_ID").copy()
+
+#adding total revenue column
+merged_df["Total_revenue"] = merged_df["Unit_Price"]*merged_df["Quantity"]
+print(merged_df)
+
