d89a2573160b9a70cb7d4da148cf58a2d3ffa80a
Anushangar-U <u.anushangar@gmail.com>
2026-02-28 20:41:07 +0530

    Merginf the dataframes

--- L4_The_National_Retail_Audit/source/final_audit.py
+++ L4_The_National_Retail_Audit/source/final_audit.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 sales_df = pd.read_csv(r"python\numpy_project\L4_The_National_Retail_Audit\data\sales_2025.csv")
 
+#DATA CLEANING
 #Filling the NA values with existing data using linear interpolate
 sales_df["Timestamp"] = pd.to_datetime(sales_df["Timestamp"])
 sales_df["Timestamp"] = sales_df["Timestamp"].interpolate(method="linear") 
@@ -11,4 +12,20 @@ sales_df["Timestamp"] = sales_df["Timestamp"].interpolate(method="linear")
 sales_df["Quantity"] = pd.to_numeric(sales_df["Quantity"])
 sales_df = sales_df[sales_df["Quantity"] >= 0]
 
-print(sales_df.sample(10))
+
+#MERGING
+#connecting product master and sales 2025
+product_df = pd.read_csv(r"python\numpy_project\L4_The_National_Retail_Audit\data\product_master.csv")
+sales_product_merge = pd.merge(sales_df,product_df,on="Product_ID")
+
+#connecting product sales merge df to the regional targets
+targets_df = pd.read_csv(r"python\numpy_project\L4_The_National_Retail_Audit\data\regional_targets.csv")
+Final_merge = pd.merge(sales_product_merge,targets_df,on="Category")
+
+#checking shapes of the DF to ensure no data lose
+if(sales_df.shape[0] == Final_merge.shape[0]):
+    print("Shapes are same")
+else:
+    print("data lose")
+    
+
